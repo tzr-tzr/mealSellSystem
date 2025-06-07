f64a4bd6a0633b120a620d069b060955187eefe9
唐孜然 <123456>
2025-06-07 13:59:29 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,13 @@
 package com.sky.mapper;
 
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
 
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -25,5 +28,10 @@ public interface DishMapper {
      */
     @AutoFill(value= OperationType.INSERT)
     void insert(Dish dish);//写xml映射文件中
-
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);//动态sql配置到xml映射文件中
 }
